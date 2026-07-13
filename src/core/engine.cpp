bb6529f1a893a5558bc0abe97ac1432614f9b380
migusmp <mmp2004sp@gmail.com>
2026-07-13 21:18:40 +0000

    black background color on engine

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -193,7 +193,7 @@ void Engine::update(float dt) {
 }
 
 void Engine::render() {
-    glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     shader->use();
